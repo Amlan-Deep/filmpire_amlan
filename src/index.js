cf62581b67f279301fb60456ea9f77c49d427747
manaschubby <manaschubby@gmail.com>
2023-05-28 10:27:38 +0530

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
-import { BrowserRouter } from 'react-router-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+import Home from "./routes/Home";
+import Movies from "./routes/Movies";
 
-document.getElementById('root'),
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<>
+				<Home />
+				<Outlet />
+			</>
+		),
+		children: [
+			{
+				path: "movies",
+				element: (
+					<>
+						<Movies />
+					</>
+				),
+			},
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<RouterProvider router={router} />
 );
